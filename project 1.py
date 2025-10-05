3a6b2d5aa3c9080284768ad8cbe117299ef1e760
宥羽陈 <youyuchen@Eileens-MacBook-Pro-8.local>
2025-10-05 10:32:18 -0400

    finish calculation 1 and part 2

--- project 1.py
+++ project 1.py
@@ -3,7 +3,9 @@
 # email: hnachen@umich.edu
 
 import os
+import csv
 import unittest
+from collections import defaultdict
 
 class Pendata():
     """
@@ -32,9 +34,11 @@ class Pendata():
 
 
 
-
     def build_data_dict(self):
-        for line in self.data[1:]:
+        """
+        cleaning data
+        """
+        for line in self.raw_data[1:]:
             line = line.strip()
             if not line:
                 continue
@@ -44,7 +48,7 @@ class Pendata():
                 continue
 
 
-            self.data_dict['num'].apppend(int(separated[0].strip('"')))
+            self.data_dict['num'].append(int(separated[0].strip('"')))
             self.data_dict['species'].append(separated[1])
             self.data_dict['island'].append(separated[2])
 
@@ -69,6 +73,125 @@ class Pendata():
                 self.data_dict['flipper length'].append(int(separated[5]))
 
             if separated[6] == 'NA':
+                self.data_dict['body mass'].append(None)
+            else:
+                self.data_dict['body mass'].append(int(separated[6]))
+    
+    ## Calculation 1: 
+    def data_species(self):
+        """
+        Groups the data by species.
+        Returns a dictionary with the grouped data.
+        """
+
+        species_group = defaultdict(lambda: {'bill lengths': [], 'bill depth': []})
+        for i in range(len(self.data_dict['species'])):
+            cur_species = self.data_dict['species'][i]
+            cur_length = self.data_dict['bill length'][i]
+            cur_depth = self.data_dict['bill depth'][i]
+
+            if cur_length is not None:
+                species_group[cur_species]['bill length'].append(cur_length)
+
+            if cur_depth is not None:
+                species_group[cur_species]['bill depth'].append(cur_depth)
+
+        return species_group
+    
+    def ave_species_group(self, groups):
+        """
+        Takes a dictionary of grouped data and calculates the averages.
+        Returns a list of result dictionaries.
+        """
+
+        final_results = []
+        for species, data in groups.items():
+            if len(data['bill length']) > 0:
+                ave_length = sum(data['bill length']) / len(data('bill length'))
+            else: 
+                ave_length = 0
+
+            if len(data['bill depth']) > 0:
+                ave_depth = sum(data['bill depth']) / len(data('bill depth'))
+            else: 
+                ave_depth = 0
+
+            result_dict = {
+                'species': species, 
+                'mean bill length(mm)': round(ave_length, 3), 
+                'mean bill depth(mm)': round(ave_depth, 3)
+
+            }
+            final_results.append(result_dict)
+        return final_results
+    
+
+    # Calculation 2: who is the strongest penguins. 
+    def cal_BMI(self):
+        """
+        Iteration all penguins and calculate their BMI scores. 
+        """
+
+        scores = []
+        for i in range(len(self.data_dict['num'])):
+            id = self.data_dict['num'][i]
+            mass = self.data_dict['body mass'][i]
+            flipper = self.data_dict['flipper length'][i]
+
+        if mass is not None and flipper is not None: 
+            mass_kg = mass / 1000
+            flipper_m = flipper / 1000
+            cur_score = mass_kg / (flipper_m * flipper_m)
+
+            scores.append({'id': id, 'score': cur_score})
+        
+        return scores
+    
+    def winner(self, score_list):
+        """
+        find the highest score from the list. 
+        """
+
+        if not score_list: 
+            return {'id': -1, 'score': -1}
+       
+        list = sorted(score_list, key=lambda penguin: penguin['score'], reverse=True)
+        champion = sorted
+    
+    def find_winner(self):
+        """
+        find all information about the winner.
+        """
+        scores = self.cal_BMI()
+        winner = self.winner(scores)
+
+        if winner['id'] == -1:
+            return {"error": "No valid penguins found"}
+        
+
+
+
+
+
+    
+    
+
+
+
+
+ 
+
+
+class Testpenguins(unittest.TestCase):
+
+
+    def main():
+
+
+
+
+
+            
 
 
             
